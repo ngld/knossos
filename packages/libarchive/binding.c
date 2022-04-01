08c10a8bb06eb0578a4b7dda5e1496cca58c3eed
ngld <ngld@tproxy.de>
2022-04-01 18:02:32 +0200

    fix(libarchive): building on unix

--- packages/libarchive/binding.c
+++ packages/libarchive/binding.c
@@ -5,17 +5,29 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 #ifdef _WIN32
 #define O_CLOEXEC 0
+#else
+#define O_BINARY 0
+#endif
+
+#ifdef __APPLE__
+#define lseek64 lseek
 #endif
 
 int libarchive_get_fd(const char *filename, char **errormsg) {
   int fd = open(filename, O_RDONLY | O_BINARY | O_CLOEXEC);
   if (fd < 0) {
     *errormsg = malloc(256);
+#ifdef _WIN32
     strerror_s(*errormsg, 256, errno);
+#else
+    strerror_r(errno, *errormsg, 256);
+#endif
   }
 
   return fd;
